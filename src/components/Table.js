b56258d31e5097ec37dda2b1e66da686081fb0c7
meganrm <meganrm@gmail.com>
2019-07-30 18:49:31 -0700

    changed make new event link

--- src/components/Table.js
+++ src/components/Table.js
@@ -49,7 +49,7 @@ class Table extends React.Component {
       return (
         <div id="events-list">
           <p className="no-results">Looks like there are no events near you right now. You can create your own
-            <a href="http://act.indivisible.org/event/local-actions/create/" target="_blank"> here.</a>
+            <a href="https://act.impeachmentaugust.org/event/register-impeachment-august/create/" target="_blank"> here.</a>
           </p>
         </div>
       );
