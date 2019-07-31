d15d258b6fee8309de2110154effb88558258956
meganrm <meganrm@gmail.com>
2019-07-30 18:49:08 -0700

    changed rsvp link

--- src/state/events/model.js
+++ src/state/events/model.js
@@ -12,15 +12,6 @@ export default class IndEvent {
   }
 
   makeUrl() {
-    if (this.displayAltLink) {
-      return this.linkToInfo;
-    }
-    if (campaignMapping[this.campaignNo] === 'mobilizeamerica-public-events' && this.linkToInfo) {
-      return this.linkToInfo;
-    }
-    const arr = this.campaign.split('/');
-    const campaignNumber = arr[arr.length - 2];
-    const campaignName = campaignMapping[campaignNumber] || 'local-actions';
-    return `http://act.indivisible.org/event/${campaignName}/${this.id}/signup/`;
+    return `http://act.impeachmentaugust.org/event/attend-impeachment-august/${this.id}`;
   }
 }
