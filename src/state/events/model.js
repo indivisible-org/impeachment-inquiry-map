114f6dbbbfaaba92857cc42b1ff1b218c886df2b
meganrm <meganrm@gmail.com>
2019-09-24 19:13:08 -0700

    change url for rsvp

--- src/state/events/model.js
+++ src/state/events/model.js
@@ -2,8 +2,12 @@ const campaignMapping = {
   15: 'local-actions',
   19: 'mobilizeamerica-public-events',
   21: 'save-scotus-actions',
+  24: 'we-are-indivisible-2020-kickoff',
+  27: 'impeachment-august',
+  28: '2020-candidate-events',
   9: 'recess-townhall',
 };
+// 15, 9, 21, 24, 27, 28
 
 export default class IndEvent {
   constructor(props) {
@@ -12,6 +16,9 @@ export default class IndEvent {
   }
 
   makeUrl() {
-    return `http://act.impeachmentaugust.org/event/attend-impeachment-august/${this.id}`;
+    const arr = this.campaign.split('/');
+    const campaignNumber = arr[arr.length - 2];
+    const campaignName = campaignMapping[campaignNumber] || 'local-actions';
+    return `https://act.impeachnow.org/event/${campaignName}/${this.id}`;
   }
 }
